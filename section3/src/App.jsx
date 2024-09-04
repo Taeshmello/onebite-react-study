085849d256a115d96b4d2526e796e8272dc4942d
Taeshmello <taeho060419@gmail.com>
2024-09-04 12:25:45 +0900

    state와 useState

--- section3/src/App.jsx
+++ section3/src/App.jsx
@@ -2,10 +2,27 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
+  const userInfo = {
+    name: "엄준식",
+    isLogin : "로그인됨",
+  }
   const [count, setCount] = useState(0);
   const [light, setLight] = useState("off");
+  const [login, setLogin] = useState(userInfo.isLogin)
+
+  
   return (
     <>
+    <div>
+    <h1>{login}</h1>
+    <button onClick={()=>{
+      if(login === "로그인됨"){
+        setLogin("로그아웃됨")
+      }else{
+        setLogin("로그인됨")
+      }
+    }}>{login == "로그인됨" ? "로그아웃하기":"로그인하기"}</button>
+    </div>
     <div>
       <h1>{light}</h1>
       <button onClick={()=>{
